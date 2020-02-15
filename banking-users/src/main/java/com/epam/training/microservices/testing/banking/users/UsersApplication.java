c857cdfe2fb20d11a51d02471f53fae9f5217503
Aleksandr Barmin <barmin.alexander@gmail.com>
2020-02-15 16:48:21 +0300

    Add a banking application

---
+++ banking-users/src/main/java/com/epam/training/microservices/testing/banking/users/UsersApplication.java
@@ -0,0 +1,11 @@
+package com.epam.training.microservices.testing.banking.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(UsersApplication.class, args);
+  }
+}
