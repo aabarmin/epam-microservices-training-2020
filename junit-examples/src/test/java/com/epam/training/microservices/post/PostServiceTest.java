78523f11629a4234e8257fb0771725d817064b1b
Aleksandr Barmin <Aleksandr.Barmin@thomsonreuters.com>
2020-05-31 16:33:55 +0300

    Adding first example for Mockito

---
+++ junit-examples/src/test/java/com/epam/training/microservices/post/PostServiceTest.java
@@ -0,0 +1,102 @@
+package com.epam.training.microservices.post;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PostServiceTest {
+  @Mock
+  private PostRepository postRepository;
+
+  @Mock
+  private PostSanitizer postSanitizer;
+
+  @InjectMocks
+  private PostService unitUnderTest;
+
+  @Test
+  public void check_contextStarts() {
+    assertThat(unitUnderTest).isNotNull();
+  }
+
+  @Test
+  public void findAll_returnPredefinedAnswer() {
+    when(postRepository.findAll()).thenReturn(Collections.singletonList(new Post()));
+
+    final List<Post> result = unitUnderTest.findAll();
+
+    assertThat(result).isNotNull();
+    assertThat(result).hasSize(1);
+  }
+
+  @Test
+  public void findOne_computeReturnValue() {
+    when(postRepository.findById(anyInt())).thenAnswer(invocationOnMock -> {
+      final Integer id = invocationOnMock.getArgumentAt(0, Integer.class);
+
+      final Post post = new Post();
+      post.setId(id);
+      return Optional.of(post);
+    });
+
+    final Post post = unitUnderTest.findOne(42);
+
+    assertThat(post).isNotNull();
+    assertThat(post.getId()).isEqualTo(42);
+  }
+
+  @Test
+  public void findOne_checkMethodCalled() {
+    when(postRepository.findById(anyInt())).thenReturn(Optional.of(new Post()));
+
+    unitUnderTest.findOne(42);
+
+    verify(postRepository, times(1)).findById(anyInt());
+  }
+
+  @Test
+  public void save_captureValues() {
+    final Post post = new Post();
+    post.setContent("This is content");
+
+    final Post result = unitUnderTest.save(post);
+
+    final ArgumentCaptor<String> contentCaptor = ArgumentCaptor.forClass(String.class);
+
+    verify(postSanitizer, times(1)).sanitize(contentCaptor.capture());
+
+    final String capturedValue = contentCaptor.getValue();
+
+    assertThat(capturedValue).isEqualTo("This is content");
+  }
+
+  @Test
+  public void save_captureOrder() {
+    final Post post = new Post();
+
+    unitUnderTest.save(post);
+
+    final InOrder order = inOrder(postSanitizer, postRepository);
+
+    order.verify(postSanitizer, times(1)).sanitize(anyString());
+    order.verify(postRepository, times(1)).save(any(Post.class));
+    // change the order to show the failure
+  }
+}
